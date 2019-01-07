042146df6c0a23c172ca716ff770d3f01258cc51
memento_code <bigbuddalive@gmail.com>
2019-01-08 00:29:03 +0200

    packet crc added

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((addr, port))
 # bitstream init
 stream = bitstream.BitStream()
+# variables
+headers_len = 17
 
 '''frame byte fields
 [0:3] preamble
@@ -43,6 +45,12 @@ def check_header_crc(data):
     return bytes.fromhex(crc) == data[14:16]
 
 
+def get_packet_crc(data):
+    crc = hex(Crc16CcittFalse.calc(data[18:510]))  # CCITT False
+    crc = crc[4] + crc[5] + crc[2] + crc[3]  # revers due to HARD realization
+    return bytes.fromhex(crc) == data[510:512], crc, data[510:512], data[18:510]
+
+
 def get_idle_channel(data):
     if bytes.fromhex('BF') == data[9:10]:
         return True
@@ -50,28 +58,41 @@ def get_idle_channel(data):
         return False
 
 
-def get_space_craft_id(data):
-    return str(data)[66:74], str(data)[74:80]
-
-
-def first_header_pointer_handler(data):
-    if bytes.fromhex('0000') == data[16:18]:
-        pass
-    elif bytes.fromhex('07FF') == data[16:18]:
-        pass
-
-
 def get_header(packet_data):
     return str(packet_data)[64:112], len(str(packet_data)[64:112]), str(packet_data)[0:64], len(str(packet_data)[0:64])
 
 
-#packet = receive_data()
-#print(packet[16:18])
-# print(check_header_crc(packet), get_idle_channel(packet))
+def get_space_craft_id(data):
+    return str(data)[66:74], str(data)[74:80]
 
 
-while True:
+def first_header_pointer_handl(data):
+    if bytes.fromhex('0000') == data[16:18]:
+        pass
+    elif bytes.fromhex('07FE') == data[16:18]:
+        pass
+    elif bytes.fromhex('07FF') == data[16:18]:
+        pass
+    else:
+        return int.from_bytes(data[16:18], byteorder='big') + headers_len
+
+
+def main():
     packet = receive_data()
     if check_header_crc(packet):
         if get_idle_channel(packet):
-            continue
+            return 'idle'
+        elif first_header_pointer_handl(packet) == 0 and True:
+            pass
+
+
+packet = receive_data()
+# print(int.from_bytes(packet[16:18], byteorder='big'))
+print(get_packet_crc(packet))
+
+
+# while True:
+#     packet = receive_data()
+#     if check_header_crc(packet):
+#         if get_idle_channel(packet):
+#             continue
